0dd037bbc4ceb9fa8295a192276c51d94b22175f
Justin Wozniak <wozniak@c10.pads.ci.uchicago.edu>
2013-10-28 13:44:30 -0500

    Initial working version

--- bin/sync3.zsh
+++ bin/sync3.zsh
@@ -1,7 +1,9 @@
-#!/bin/zsh
+#!/bin/zsh -eu
 
-# LOG=~wozniak/proj/d-e/logs/sync3.log
-LOG=/dev/stdout
+LOG=~wozniak/proj/d-e/logs/sync3.log
+# LOG=/dev/stdout
+
+mkdir -p $( dirname ${LOG} )
 
 SITE1="wozniak#wozniak-laptop-u"
 SITE2="lcrc#fusion"
@@ -12,17 +14,20 @@ GO=cli.globusonline.org
 ACCT=${GO_USER}@${GO}
 
 # Sync on file size:
-XFER_ARGS=( --preserve-mtime -s 1 )
+XFER_ARGS=( -r --preserve-mtime -s 1 )
 
 date_word()
-# 2006-10-13_14:26 : Good for file names
+# 2006-10-13_14-26 : Good for file names
 {
-  date "+%Y-%m-%d_%H:%M"
+  date "+%Y-%m-%d_%H-%M"
 }
 
 while (( 1 ))
 do
+  print
   LABEL=( --label $( date_word ) )
-  ssh ${ACCT} scp ${SITE1}:de.data ${SITE2}:
-  sleep 60
-done >> ${LOG}
+  print "Starting sync: ${LABEL}"
+  ssh ${ACCT} scp ${XFER_ARGS} ${LABEL} ${SITE1}:d-e.d ${SITE2}:d-e.d
+  ssh ${ACCT} scp ${XFER_ARGS} ${LABEL} ${SITE2}:d-e.d ${SITE3}:d-e.d
+  sleep ${DELAY}
+done < /dev/null >> ${LOG}
