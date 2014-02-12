22bb37fe14665564daa5b66447d297398d426ec7
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-02-12 11:29:50 -0600

    Use input file instead of hard-coded list

--- swift/merge-all.swift
+++ swift/merge-all.swift
@@ -5,6 +5,7 @@
    Run this from the top-level directory.
 */
 
+import files;
 import io;
 import string;
 import sys;
@@ -24,39 +25,45 @@ app merge_tiffs(string directory, file background, string arguments[])
 
 main
 {
+  list = argv("list");
   data = argv("data");
   printf("merge-all.swift in %s", data);
 
-  string matt[] =
-    [
-      "lsmo34_090k",
-                   // "lsmo39_130k", (broken - sent email to Ray)
-                   "lsmo42_150k",
-                  "lsmo48_090k"
-                 // "lsmo56_100k",
-                 // "lsmo52_195k",
-                 // "lsmo50_220k"
-     ];
+  string work[] = split(read(input(list)), "\n");
 
-  foreach directory in matt
+  // string matt[] =
+  //   [
+  //     // "lsmo34_090k",
+  //                  // "lsmo39_130k", (broken - sent email to Ray)
+  //     //             "lsmo42_150k",
+  //     //            "lsmo48_090k"
+  //                 "lsmo56_100k",
+  //                 "lsmo52_195k",
+  //                 "lsmo50_220k"
+  //    ];
+
+  foreach directory in work
   {
-    file background<data/directory/"darkbeg.nxs"> =
+    if (directory != "")
+    {
+      file background<data/directory/"darkbeg.nxs"> =
       merge_tiffs_background(directory, split("-p darkbeg", " "));
-    merge_tiffs(directory, background,
-                split("-p fb1scan -b darkbeg -o 0 -s 0.1 -r", " "));
-    merge_tiffs(directory, background,
-                split("-p ff1scan -b darkbeg -o 0 -s 0.1", " "));
-    merge_tiffs(directory, background,
-                split("-p ff2scan -b darkbeg -o 0 -s 0.1", " "));
-    merge_tiffs(directory, background,
-                split("-p fb2scan -b darkbeg -o 0 -s 0.1 -r", " "));
-    merge_tiffs(directory, background,
-                split("-p sfscan -b darkbeg -o 0 -s 0.1", " "));
-    merge_tiffs(directory, background,
-                split("-p sbscan -b darkbeg -o 0 -s 0.1 -r", " "));
-    merge_tiffs(directory, background,
-                split("-p ubfscan -b darkbeg -o 0 -s 0.1", " "));
-    merge_tiffs(directory, background,
-                split("-p ubbscan -b darkbeg -o 0 -s 0.1 -r", " "));
+      merge_tiffs(directory, background,
+                  split("-p fb1scan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p ff1scan -b darkbeg -o 0 -s 0.1 -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p ff2scan -b darkbeg -o 0 -s 0.1 -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p fb2scan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p sfscan -b darkbeg -o 0 -s 0.1 -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p sbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p ubfscan -b darkbeg -o 0 -s 0.1 -c", " "));
+      merge_tiffs(directory, background,
+                  split("-p ubbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+    }
   }
 }
