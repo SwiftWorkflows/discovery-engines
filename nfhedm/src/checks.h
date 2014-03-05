1cb6f57aac537639ca730f390487c2780009f176
jmjwozniak <wozniak@mcs.anl.gov>
2014-03-05 16:33:11 -0600

    The error check

---
+++ nfhedm/src/checks.h
@@ -0,0 +1,21 @@
+/*
+ * checks.h
+ *
+ *  Created on: Mar 5, 2014
+ *      Author: wozniak
+ */
+
+#ifndef CHECKS_H
+#define CHECKS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void file_not_found(const char* filename)
+{
+    printf("Could not find file: %s\n", filename);
+    fflush(NULL);
+    exit(EXIT_FAILURE);
+}
+
+#endif
