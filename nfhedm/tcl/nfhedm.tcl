5753dbdb55115f4733236ff559b0b27e2234a7af
jmjwozniak <wozniak@mcs.anl.gov>
2014-03-31 15:04:49 -0500

    Report output result in lib

--- nfhedm/tcl/nfhedm.tcl
+++ nfhedm/tcl/nfhedm.tcl
@@ -11,5 +11,12 @@ namespace eval tclfop {
         set result [ blobutils_malloc [ expr 4 * [ blobutils_sizeof_float ] ] ]
         set result [ blobutils_cast_to_dbl_ptr $result ]
         FitOrientation_Swift "data/Parameters2.txt" 1 $result
+        
+        for { set i 0 } { $i < 4 } { incr i } { 
+            set v [ blobutils_get_float $result $i ] 
+            puts "result\[$i\]: $v"
+        }
+
+        blobutils_free $result
     }
 }
