161f144dd3dae611db26d976f49249ea5f77fdc2
Justin M Wozniak <wozniak@mcs.anl.gov>
2014-04-08 12:55:06 -0500

    Add nxmax, nxfind to workflow

--- swift/merge-all.swift
+++ swift/merge-all.swift
@@ -17,10 +17,21 @@ app (file background) merge_tiffs_background(string directory,
     "-d" directory arguments;
 }
 
-app merge_tiffs(string directory, file background, string arguments[])
+app (file nxs) merge_tiffs(string directory, file background,
+                           string arguments[])
 {
   "/home/wozniak/proj/d-e/bin/merge-tiffs.sh" background
     "-d" directory arguments;
+}
+
+app (file nxmax_out) nxmax(string directory, file nxs)
+{
+  "/home/wozniak/proj/d-e/bin/nxmax.sh" directory nxs nxmax_out;
+}
+
+app (file nxfind_out) nxfind(string directory, file nxs, file nxmax_out)
+{
+  "/home/wozniak/proj/d-e/bin/nxfind.sh" directory nxs nxmax_out nxfind_out;
 }
 
 main
@@ -31,23 +42,15 @@ main
 
   string work[] = split(read(input(list)), "\n");
 
-  // string matt[] =
-  //   [
-  //     // "lsmo34_090k",
-  //                  // "lsmo39_130k", (broken - sent email to Ray)
-  //     //             "lsmo42_150k",
-  //     //            "lsmo48_090k"
-  //                 "lsmo56_100k",
-  //                 "lsmo52_195k",
-  //                 "lsmo50_220k"
-  //    ];
-
   foreach directory in work
   {
     if (directory != "")
     {
+      // Create background NXS:
       file background<data/directory/"darkbeg.nxs"> =
-      merge_tiffs_background(directory, split("-p darkbeg -c", " "));
+        merge_tiffs_background(directory, split("-p darkbeg -c", " "));
+
+      // Merge each scan:
       merge_tiffs(directory, background,
                   split("-p fb1scan -b darkbeg -o 0 -s 0.1 -r -c", " "));
       merge_tiffs(directory, background,
@@ -60,10 +63,26 @@ main
                   split("-p sfscan -b darkbeg -o 0 -s 0.1 -c", " "));
       merge_tiffs(directory, background,
                   split("-p sbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
-      merge_tiffs(directory, background,
-                  split("-p ubfscan -b darkbeg -o 0 -s 0.1 -c", " "));
-      merge_tiffs(directory, background,
-                  split("-p ubbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+
+      // Merge the UB scans:
+      file ubf_nxs<data/directory/"ubfscan.nxs"> =
+        merge_tiffs(directory, background,
+                    split("-p ubfscan -b darkbeg -o 0 -s 0.1 -c", " "));
+      file ubb_nxs<data/directory/"ubbscan.nxs"> =
+        merge_tiffs(directory, background,
+                    split("-p ubbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
+
+      // NXmax:
+      file ubf_nxmax_out<data/directory/"ubf.nxmax.out"> =
+         nxmax(directory, ubf_nxs);
+      file ubb_nxmax_out<data/directory/"ubb.nxmax.out"> =
+        nxmax(directory, ubb_nxs);
+
+      // NXfind:
+       file ubf_nxfind_out<data/directory/"ubf.nxfind.out"> =
+         nxfind(directory, ubf_nxs, ubf_nxmax_out);
+      file ubb_nxfind_out<data/directory/"ubb.nxfind.out"> =
+        nxfind(directory, ubb_nxs, ubb_nxmax_out);
     }
   }
 }
