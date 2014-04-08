50e1b98971b2d6c68baa940d1b81eb2aa8a76031
jmjwozniak <wozniak@mcs.anl.gov>
2014-04-08 14:12:52 -0500

    Use catalog.py

--- nexus/bin/catalog-delete-dataset.zsh
+++ nexus/bin/catalog-delete-dataset.zsh
@@ -1,11 +1,9 @@
 #!/bin/zsh -eu
 
-alias catalog='python ~/.local/lib/python2.6/site-packages/globusonline/catalog/client/examples/catalog.py'
-
 if [[ ${#*} != 2 ]]
 then
   print "usage catalog-delete-dataset.zsh <CATALOG> <DATASET>"
   exit 1
 fi
 
-catalog delete_dataset $1 $2 true
+catalog.py delete_dataset $1 $2 true
