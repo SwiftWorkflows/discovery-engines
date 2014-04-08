7effacbba0915065aea730519ff8b666119a5e46
jmjwozniak <wozniak@mcs.anl.gov>
2014-04-08 14:12:38 -0500

    Minor fixes

--- nexus/bin/catalog-dataset-ids.zsh
+++ nexus/bin/catalog-dataset-ids.zsh
@@ -1,9 +1,15 @@
 #!/bin/zsh -eu
 
-if [[ -z ${DEFAULT_CATALOG_ID} ]]
+if [[ -z ${GCAT_DEFAULT_CATALOG_ID} ]]
 then
-  print "Set DEFAULT_CATALOG_ID"
+  print "Set GCAT_DEFAULT_CATALOG_ID"
   exit 1
 fi
 
-catalog.py get_datasets ${DEFAULT_CATALOG_ID} -t | clm 1 | grep -E "[[:digit:]]+" | sed "s/)//"
+catalog.py get_datasets -t | clm 1 | grep -E "[[:digit:]]+" | sed "s/)//"
+if [[ ${pipestatus[1]} != 0 ]]
+then
+  print "catalog operation failed!"
+  exit 1
+fi
+exit 0
