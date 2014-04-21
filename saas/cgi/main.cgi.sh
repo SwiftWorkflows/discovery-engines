55a39e99072a8bb0af0ba72e9294accb37664b9f
jmjwozniak <wozniak@mcs.anl.gov>
2014-04-21 16:02:06 -0500

    Check in - switching to Galaxy

--- saas/cgi/main.cgi.sh
+++ saas/cgi/main.cgi.sh
@@ -22,8 +22,8 @@ then
   mkdir -p $SESSION_D
   export current_directory="/tmp"
   echo $current_directory  > $SESSION_D/current_directory.txt
-  date "+%Y/%m/%d %I:%M%p" > $SESSION_D/created-human.txt
-  date "+%s"               > $SESSION_D/created-seconds.txt
+  date "+%Y/%m/%d %I:%M%p" > $SESSION_D/created_human.txt
+  date "+%s"               > $SESSION_D/created_seconds.txt
 fi
 
 SESSION_D=$SESSIONS/$session.d
@@ -32,10 +32,10 @@ if [[ $current_directory == "none" ]]
 then
   current_directory=$( cat $SESSION_D/current_directory.txt )
 fi
-
-echo session: $session
-echo current
-
-echo 
+export created_human=${created_human:-none}
+if [[ $created_human == "none" ]] 
+then
+  created_human=$( cat $SESSION_D/created_human.txt )
+fi
 
 m4 -P $JWWW_ROOT/html/main.html
