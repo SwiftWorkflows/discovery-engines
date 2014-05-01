b6fe079f95a67686cdc27499d33673dc867729bb
jwozniak <wozniak@mcs.anl.gov>
2014-04-30 21:42:48 -0500

    Working script to convert all files

--- nfhedm/swift/convert-bin-l2n.swift
+++ nfhedm/swift/convert-bin-l2n.swift
@@ -1,14 +1,23 @@
 
-import sys;
 import io;
 import files;
+import string;
+import sys;
+
+app (file o) convert(file i)
+{
+  "convert-bin-l2n" @i @o;
+}
 
 main
 {
   string data = argv("data");
-  file F[] = glob(data/"*.bin*");
+  file F[] = glob(data/"*bin*");
   foreach f in F
   {
     printf("file: %s", filename(f));
+    s = replace(filename(f), "data", "convert", 0);
+    printf(s);
+    file o<s> = convert(f);
   }
 }
