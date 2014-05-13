3a84deba89035d362d34f85c27632016d5975b59
jmjwozniak <wozniak@mcs.anl.gov>
2014-05-13 13:33:43 -0500

    Allow easier path change

--- nfhedm/swift/ImageProcessing.sh
+++ nfhedm/swift/ImageProcessing.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-/clhome/TOMO1/PeaksAnalysisHemant/NF_HEDM/./ImageProcessing $1 $2 $3
-RC=$?
+# NFHEDM=/clhome/TOMO1/PeaksAnalysisHemant/NF_HEDM
+NFHEDM=/home/wozniak/sfw/nfhedm/bin
+${NFHEDM}/ImageProcessing $1 $2 $3
+RC=${?}
 if [[ $RC != 0 ]] 
 then 
     echo "Warning: RC=0"
--- nfhedm/swift/MedianImage.sh
+++ nfhedm/swift/MedianImage.sh
@@ -3,4 +3,7 @@
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/clhome/TOMO1/PeaksAnalysisHemant/DIPLIB/Linuxa64/lib/
 export LD_LIBRARY_PATH
 
-/clhome/TOMO1/PeaksAnalysisHemant/NF_HEDM/./MedianImage $1 $2
+# NFHEDM=/clhome/TOMO1/PeaksAnalysisHemant/NF_HEDM
+NFHEDM=/home/wozniak/sfw/nfhedm/bin
+
+${NFHEDM}/MedianImage $1 $2
