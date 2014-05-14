baeacbee4b9e1530388f6a92929bc166b7b17212
jmjwozniak <wozniak@mcs.anl.gov>
2014-05-14 15:40:06 -0500

    Sketch to run NFHEDM/Swift/T remotely

---
+++ nfhedm/e2e/tests/test-ssh/run-remote-swift-t.zsh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+set -x
+
+START=$1
+STOP=$2
+
+REMOTE_HOST=cetus.alcf.anl.gov
+REMOTE_USER=wozniak
+REMOTE=${REMOTE_USER}@${REMOTE_HOST}
+
+REMOTE_HOME=/home/${REMOTE_USER}
+REMOTE_SCRIPT=${REMOTE_HOME}/proj/ppc64/d-e/nfhedm/swift/FitOrientation-T.sh
+
+REMOTE_DATA=${REMOTE_HOME}/Au_Reduced2-converted
+REMOTE_PARAMS=${REMOTE_DATA}/ParametersGoldApril14.txt
+
+ssh ${REMOTE} ${REMOTE_SCRIPT}  ${REMOTE_DATA} ${REMOTE_PARAMS} ${START} ${STOP}
