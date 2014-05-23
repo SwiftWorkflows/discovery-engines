d9bc075c1f6a1da31447cb8efe69fe485df5be5f
jmjwozniak <wozniak@mcs.anl.gov>
2014-05-23 11:42:33 -0500

    New function crash()

--- nfhedm/src/checks.h
+++ nfhedm/src/checks.h
@@ -8,8 +8,18 @@
 #ifndef CHECKS_H
 #define CHECKS_H
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+static void crash(const char* format, ...)
+{
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+    exit(EXIT_FAILURE);
+}
 
 static void file_not_found(const char* filename)
 {
