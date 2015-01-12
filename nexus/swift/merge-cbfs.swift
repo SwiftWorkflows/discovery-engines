c23b8fea171a04a99daf7ff0eab1a47dd81f5473
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-01-12 14:41:58 -0600

    Script used to merge CBFs

--- nexus/swift/merge-cbfs.swift
+++ nexus/swift/merge-cbfs.swift
@@ -10,79 +10,37 @@ import io;
 import string;
 import sys;
 
-app (file background) merge_cbfs_background(string directory,
-                                             string arguments[])
+app merge_cbfs(string arguments[])
+// data: I.e., /home/bessrc/sharedbigdata/data1/osborn-2014-1/bfap00
 {
-  "/home/wozniak/proj/d-e/nexus/bin/merge-cbfs.sh" background
-    "-d" directory arguments;
-}
-
-app (file nxs) merge_cbfs(string directory, file background,
-                           string arguments[])
-{
-  "/home/wozniak/proj/d-e/nexus/bin/merge-cbfs.sh" background
-    "-d" directory arguments;
-}
-
-app (file nxmax_out) nxmax(string directory, file nxs)
-{
-  "/home/wozniak/proj/d-e/nexus/bin/nxmax.sh" directory nxs nxmax_out;
-}
-
-app (file nxfind_out) nxfind(string directory, file nxs, file nxmax_out)
-{
-  "/home/wozniak/proj/d-e/nexus/bin/nxfind.sh" directory nxs nxmax_out nxfind_out;
+  // nxmerge -d f1 -p scan -e cbf -o f1
+  "/home/wozniak/proj/d-e/nexus/bin/nxmerge.sh" arguments;
 }
 
 main
 {
-  list = argv("list");
   data = argv("data");
-  printf("merge-all.swift in %s", data);
+  printf("merge-cbfs.swift in %s", data);
 
-  string work[] = split(read(input(list)), "\n");
+  file kts[] = glob(data/"kt*");
 
-  foreach directory in work
+  foreach kt in kts
   {
-    if (directory != "")
+    printf("kt: %s", filename(kt));
+    file temperatures[] = glob(filename(kt)/"*K");
+    foreach temperature in temperatures
     {
-      // Create background NXS:
-      file background<data/directory/"darkbeg.nxs"> =
-        merge_cbfs_background(directory, split("-p darkbeg -c", " "));
+      printf("temperature: %s", filename(temperature));
 
       // Merge each scan:
-      merge_cbfs(directory, background,
-                  split("-p fb1scan -b darkbeg -o 0 -s 0.1 -r -c", " "));
-      merge_cbfs(directory, background,
-                  split("-p ff1scan -b darkbeg -o 0 -s 0.1 -c", " "));
-      merge_cbfs(directory, background,
-                  split("-p ff2scan -b darkbeg -o 0 -s 0.1 -c", " "));
-      merge_cbfs(directory, background,
-                  split("-p fb2scan -b darkbeg -o 0 -s 0.1 -r -c", " "));
-      merge_cbfs(directory, background,
-                  split("-p sfscan -b darkbeg -o 0 -s 0.1 -c", " "));
-      merge_cbfs(directory, background,
-                  split("-p sbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
-
-      // Merge the UB scans:
-      file ubf_nxs<data/directory/"ubfscan.nxs"> =
-        merge_cbfs(directory, background,
-                    split("-p ubfscan -b darkbeg -o 0 -s 0.1 -c", " "));
-      file ubb_nxs<data/directory/"ubbscan.nxs"> =
-        merge_cbfs(directory, background,
-                    split("-p ubbscan -b darkbeg -o 0 -s 0.1 -r -c", " "));
-
-      // NXmax:
-      file ubf_nxmax_out<data/directory/"ubf.nxmax.out"> =
-         nxmax(directory, ubf_nxs);
-      file ubb_nxmax_out<data/directory/"ubb.nxmax.out"> =
-        nxmax(directory, ubb_nxs);
-
-      // NXfind:
-       file ubf_nxfind_out<data/directory/"ubf.nxfind.out"> =
-         nxfind(directory, ubf_nxs, ubf_nxmax_out);
-      file ubb_nxfind_out<data/directory/"ubb.nxfind.out"> =
-        nxfind(directory, ubb_nxs, ubb_nxmax_out);
+      foreach f in [1:2]
+      {
+        directory = "%s/f%i"     % (filename(temperature), f);
+        output    = "%s/f%i.nxs" % (filename(temperature), f);
+        merge_cbfs(split("-d " + directory + " -o " + output +
+                         " -p scan -e cbf",
+                         " "));
+      }
     }
   }
 }
