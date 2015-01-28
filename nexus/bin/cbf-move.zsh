b1a16f01c443ee8cf41f94c47738a4ab1eba29cb
jwozniak <wozniak@mcs.anl.gov>
2015-01-28 13:59:11 -0600

    New version based on re-rsync

--- nexus/bin/cbf-move.zsh
+++ nexus/bin/cbf-move.zsh
@@ -2,28 +2,42 @@
 
 set -eu
 
-RAMDISK=/ramdisk
-INBOX=${RAMDISK}/inbox
-OUTBOX=${RAMDISK}/outbox
+usage()
+{
+  print "usage: cbf-move <OPTS>* <DIRECTORY> <TARGET>"
+  print "\t OPTS:      -h for help"
+  print "\t DIRECTORY: the local directory"
+  print "\t TARGET:    the rsync target, i.e., user@host:/dirs"
+}
+
+zparseopts -D -E h=H
+if [[ ${#H} != 0 ]]
+then
+  usage 
+  exit 0
+fi
+
+if [[ ${#*} != 1 ]]
+then
+  usage
+  exit 1
+fi
+
+DIRECTORY=$1
+TARGET=$2
 
 REMOTE_HOST=nxrs.msd.anl.gov
-REMOTE_DIR=/home/bessrc/data1/2015-1
+REMOTE_DIR=/home/bessrc/sharedbigdata/data1/osborn-2015-1
 
-mkdir -p ${OUTBOX}
+alias RSYNC='rsync -rtv --exclude "*tmp"'
 
+DONE=0
 while true
 do
-  # Poll loop
-  while true
-  do
-    FILES=( $( ls ${INBOX} ) ) 
-    if (( ${#FILES} > 0 )) 
-    then
-      mv -v ${FILES} ${OUTBOX}
-      break
-    else
-      sleep 10
-    fi
-  done
-  xfer.zsh ${REMOTE_HOST} ${REMOTE_DIR} ${FILES} &
+  [[ -f ${DIRECTORY}/done.txt ]] && DONE=1
+  RSYNC ${DIRECTORY} ${REMOTE_HOST}:${REMOTE_DIR}
+  (( DONE )) && break
+  sleep 10
 done
+
+print rm -r ${DIRECTORY}
