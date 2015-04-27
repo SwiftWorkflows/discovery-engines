3ee5a94c2820572eb0389dfd3419169595e12ce0
jmjwozniak <wozniak@mcs.anl.gov>
2015-04-27 12:39:15 -0500

    Finish up this program

--- nexus/bin/catalog-list-files.py
+++ nexus/bin/catalog-list-files.py
@@ -6,17 +6,13 @@ catalog_id = os.getenv("GCAT_DEFAULT_CATALOG_ID")
 
 from globusonline.catalog.client.goauth import get_access_token
 from globusonline.catalog.client.dataset_client import DatasetClient
-
-print catalog_id
+from globusonline.catalog.client.catalog_wrapper import CatalogWrapper
+from globusonline.catalog.client.queries import Queries
 
 if __name__ == "__main__":
-    goauth_result = get_access_token()
-    client = DatasetClient(goauth_result.token, base_url=BASE_URL)
-    _, datasets = client.get_datasets()
-    for c in catalogs:
-        print cat
-        print "==== %s ====" % cat["config"]["name"]
-        for k, v in cat["config"].iteritems():
-            if k == "name":
-                continue
-            print "%s: %s" % (k, v)
+    wrap = CatalogWrapper(token_file=CatalogWrapper.default_token_file())
+    client = wrap.catalogClient
+    queries = Queries(client, catalog_id)
+    files = queries.list_files(catalog_id)
+    for f in files:
+        print f
