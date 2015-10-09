f3a232660c3f0ee43b54dd8421b52d1b2de098de
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-10-09 11:02:30 -0400

    Adding src/mt2/module.mk

---
+++ mt2/src/mt2/module.mk
@@ -0,0 +1,13 @@
+
+THIS := src/mt2
+
+BINS += bin/mt2
+
+bin/mt2: $(THIS)/mt2.o
+	mkdir -pv bin
+	gcc -o $(@) $(<) \
+		-L $(HDF)/lib -l hdf5 -l tiff \
+		-Wl,-rpath -Wl,$(HDF)/lib
+
+clean::
+	rm -fv $(THIS)/*.o
