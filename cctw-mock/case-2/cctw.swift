05254db2e7910ef9f7af7bfb5a9e3fe5bea560b0
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-10-13 10:01:15 -0400

    Clean up

--- cctw-mock/case-2/cctw.swift
+++ cctw-mock/case-2/cctw.swift
@@ -10,18 +10,8 @@ in_entry = argv("in_entry"); // in h5_path
 out_file = argv("o"); 
 out_entry = argv("out_entry"); // out h5_path
 
-fname = argv("m");
-fn_s = argv("f");
-
-fn = toint(fn_s);
-
-app qsub(string line[])
-{
-  "qsub-block" line;
-}
-
 app (void o)
-cctw(string directory, string jobname, string command[], int threads)
+cctw_xform(string directory, string jobname, string command[], int threads)
 {
   "qsub-block" "-j" threads "--" directory jobname 
     command;
@@ -49,15 +39,13 @@ foreach i in [0:S-1]
                         "-S", subset_arg,
                         "-j", toString(threads) ];
   
-  void o = cctw(getenv("PWD"), "cctw-%i"%i,
-                command,
-                threads);
+  void o = cctw_xform(getenv("PWD"), "cctw-%i"%i,
+                      command,
+                      threads);
   o => A[i] = 0;
 }
 
-/*
 wait deep (A)
 {
   cctw_merge(getenv("PWD"), "cctw-merge.zsh", out_file);
 }
-*/
