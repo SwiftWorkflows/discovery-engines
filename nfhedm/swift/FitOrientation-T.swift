3ca63e8b8998557dabdad77a4cd70177730d3806
jmjwozniak <wozniak@mcs.anl.gov>
2016-01-29 13:38:41 -0600

    Drop bulk data

--- nfhedm/swift/FitOrientation-T.swift
+++ nfhedm/swift/FitOrientation-T.swift
@@ -6,10 +6,18 @@ import sys;
  @param rown Integer in [1,270]
 */
 @dispatch=WORKER
-  (int o) FitOrientation_Swift(string ParameterFileName, int rown,
-                               string MicrostructureFileName)
+  (float result[]) FitOrientation_Swift(string ParameterFileName, int rown)
 "tclnfhedm" "0.0"
-[ "set <<o>> [ FitOrientation_Swift <<ParameterFileName>> <<rown>> <<MicrostructureFileName>>]" ];
+[
+----
+    set s [ blobutils_malloc 160 ]
+    set s [ blobutils_cast_to_char_ptr $s ]    
+    set b [ FitOrientation_Swift <<ParameterFileName>> <<rown>> $s ]
+    if { ! b } { puts "ERROR in FitOrientation!" }
+    puts "result: $s"
+    blob_free $s    
+----
+];
 
 main
 {
@@ -22,6 +30,6 @@ main
          ParameterFileName, start, end);
   foreach rown in [start:end] {
     // printf("rown: %i", rown);
-    FitOrientation_Swift(ParameterFileName, rown, MicrostructureFileName);
+    FitOrientation_Swift(ParameterFileName, rown);
   }
 }
