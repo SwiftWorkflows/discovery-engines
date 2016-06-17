45c3ef2b0ff7c1f7bf3571f58eade33d0eeef137
Justin M Wozniak <wozniak@mcs.anl.gov>
2016-06-17 14:10:30 -0500

    Add mu

--- sro/sro.f90
+++ sro/sro.f90
@@ -1,19 +1,22 @@
 
 subroutine compute_I_sro(a_o1v1, a_o2v2, a_o1v2, &
+                         mu1, mu2, mu3, &
                          h10, h20, h30, h11, h21, h31, h1n, h2n, h3n, &
                          intensity)
 
   real*8,  intent(in) :: a_o1v1, a_o2v2, a_o1v2
+  real*8,  intent(in) :: mu1, mu2, mu3
   real*8,  intent(in) :: h10, h20, h30 ! Lower boundary of h1,2,3
   real*8,  intent(in) :: h11, h21, h31 ! Upper boundary of h1,2,3
   integer, intent(in) :: h1n, h2n, h3n ! Discretization of h1,2,3
 
   real*8, intent(out) :: intensity(h1n, h2n, h3n)
 
-  real*8 :: I1, I2, I12
+  ! Locals
+  real*8  :: I1, I2, I12
   integer :: i, j, k
   integer :: l, m, n
-  real*8 :: h1step, h2step, h3step, h1, h2, h3, a
+  real*8  :: h1step, h2step, h3step, h1, h2, h3, a
 
   h1step = h10 + (h11-h10)/h1n
   h2step = h20 + (h21-h20)/h2n
@@ -29,7 +32,7 @@ subroutine compute_I_sro(a_o1v1, a_o2v2, a_o1v2, &
            I1  = a_o1v1 * cos(a)
            I2  = a_o2v2 * cos(a)
            I12 = a_o1v2 * cos(a)
-           intensity(i,j,k) = I1 + I2 + I12
+           intensity(i,j,k) = mu1*I1 + mu2*I2 + mu3*I12
         end do
      end do
   end do
