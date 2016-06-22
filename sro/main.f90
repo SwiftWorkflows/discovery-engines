9696e9226162fc177805c8119835e1139a639c2a
Justin Wozniak <wozniak@mcs.anl.gov>
2016-06-22 17:19:07 -0500

    Complete result- need to fix parameters

--- sro/main.f90
+++ sro/main.f90
@@ -1,22 +1,60 @@
 
-program sro
+program main
 
-  real*8 :: l, m, n
+  use SRO
 
-  call scan_command_line(l, m, n)
+  type(problem) :: p
 
-  print '(A,3F6.1)', 'l,m,n = ', l, m, n
+  character (len=1024) output_file
+
+  real*8, allocatable :: intensity(:,:,:)
+
+  call scan_command_line(p%l, p%m, p%n, output_file)
+
+  p%h10 = -10
+  p%h20 = -10
+  p%h30 = -10
+
+  p%h11 = 10
+  p%h21 = 10
+  p%h31 = 10
+
+  p%h1n = 100
+  p%h2n = 100
+  p%h3n = 100
+
+  allocate(intensity(p%h1n, p%h2n, p%h3n))
+
+  p%a_o1v1 = 1
+  p%a_o2v2 = 1
+  p%a_o1v2 = 1
+
+  p%mu1 = 1
+  p%mu2 = 1
+  p%mu3 = 1
+
+  call compute_I(p, intensity)
+
+  call write_intensity(p, intensity, output_file)
+
+  deallocate(intensity)
 
 end program
 
-subroutine scan_command_line(l, m, n)
+subroutine scan_command_line(l, m, n, output_file)
 
   real*8, intent(out) :: l, m, n
-  integer status
-  character (len=32) s
+  character (len=*), intent(out) :: output_file
 
-  if (command_argument_count() /= 3) then
-     call crash("Requires 3 arguments!")
+  integer status, required_arguments
+  character (len=32) s
+  character (len=32) message
+
+  required_arguments = 4
+  if (command_argument_count() /= required_arguments) then
+     write(message, '(A,I2)') &
+          'Required arguments:', required_arguments
+     call crash(message)
   endif
 
   call get_command_argument(1, s)
@@ -25,7 +63,7 @@ subroutine scan_command_line(l, m, n)
      call crash("Bad argument for l: " // s)
   endif
   call get_command_argument(2, s)
-  read(s,  *, iostat=status) m
+  read(s, *, iostat=status) m
   if (status > 0) then
      call crash("Bad argument for m: " // s)
   endif
@@ -35,6 +73,62 @@ subroutine scan_command_line(l, m, n)
      call crash("Bad argument for n: " // s)
   endif
 
+  call get_command_argument(4, output_file)
+
+  print '(A,3F6.1)', 'l,m,n = ', l, m, n
+
+end subroutine
+
+subroutine write_intensity(p, intensity, output_file)
+
+  use HDF5
+  use SRO
+
+  type(problem), intent(in) :: p
+  character (len=*), intent(in) :: output_file
+  real*8 :: intensity(p%h1n,p%h2n,p%h3n)
+
+  integer file_id,space_id,dset_id
+  character (len=1024) :: hdf_path
+  integer hdferr             ! Error code for HDF5
+  integer(hsize_t), dimension(1:3) :: dims
+
+  print *, "writing to: ", trim(output_file)
+  print *, p%h1n, p%h2n, p%h3n
+
+  hdf_path = "intensity"
+  dims(1) = p%h1n
+  dims(2) = p%h2n
+  dims(3) = p%h3n
+
+  print *, intensity(1,1,1)
+
+  ! Open output file
+  call h5open_f(hdferr)
+  call h5_error_check(hdferr)
+  call h5fcreate_f(output_file, H5F_ACC_TRUNC_F, file_id, hdferr, &
+       H5P_DEFAULT_F, H5P_DEFAULT_F)
+  call h5_error_check(hdferr)
+  ! Create HDF path
+  call h5screate_simple_f(3, dims, space_id, hdferr)
+  call h5_error_check(hdferr)
+  call h5dcreate_f(file_id, hdf_path, H5T_IEEE_F64LE, space_id, &
+       dset_id, hdferr)
+  call h5_error_check(hdferr)
+
+  call h5dwrite_f(dset_id, H5T_IEEE_F64LE, intensity, dims, &
+       hdferr)
+  call h5_error_check(hdferr)
+  print *, 'write done.'
+
+end subroutine
+
+subroutine h5_error_check(hdferr)
+  integer hdferr
+  if (hdferr < 0) then
+     write (*,*) 'Some HDF operation failed'
+     call exit(1)
+  end if
 end subroutine
 
 subroutine crash(message)
--- sro/sro.f90
+++ sro/sro.f90
@@ -1,41 +1,46 @@
 
-subroutine compute_I_sro(a_o1v1, a_o2v2, a_o1v2, &
-                         mu1, mu2, mu3, &
-                         l, m, n, &
-                         h10, h20, h30, h11, h21, h31, h1n, h2n, h3n, &
-                         intensity)
+module SRO
+  type problem
+     real*8  :: a_o1v1, a_o2v2, a_o1v2
+     real*8  :: mu1, mu2, mu3
+     real*8  :: l, m, n
+     real*8  :: h10, h20, h30 ! Lower boundary of h1,2,3
+     real*8  :: h11, h21, h31 ! Upper boundary of h1,2,3
+     integer :: h1n, h2n, h3n ! Discretization of h1,2,3
+  end type
 
-  real*8,  intent(in) :: a_o1v1, a_o2v2, a_o1v2
-  real*8,  intent(in) :: mu1, mu2, mu3
-  intent(in)          :: l, m, n
-  real*8,  intent(in) :: h10, h20, h30 ! Lower boundary of h1,2,3
-  real*8,  intent(in) :: h11, h21, h31 ! Upper boundary of h1,2,3
-  integer, intent(in) :: h1n, h2n, h3n ! Discretization of h1,2,3
+  contains
 
-  real*8, intent(out) :: intensity(h1n, h2n, h3n)
+    subroutine compute_I(p, intensity)
 
-  ! Locals
-  real*8  :: I1, I2, I12
-  integer :: i, j, k
-  integer :: l, m, n
-  real*8  :: h1step, h2step, h3step, h1, h2, h3, a
+      type(problem) :: p
+      real*8 :: intensity(p%h1n,p%h2n,p%h3n)
+      ! Locals
+      real*8  :: I1, I2, I12
+      integer :: i, j, k
+      real*8  :: h1step, h2step, h3step, h1, h2, h3, a
 
-  h1step = h10 + (h11-h10)/h1n
-  h2step = h20 + (h21-h20)/h2n
-  h3step = h30 + (h31-h10)/h3n
+      h1step = (p%h11 - p%h10) / p%h1n
+      h2step = (p%h21 - p%h20) / p%h2n
+      h3step = (p%h31 - p%h10) / p%h3n
 
-  do k = 1,h3n
-     h3 = h30 + k * h3step
-     do j = 1, h2n
-        h2 = h20 + j * h2step
-        do i = 1, h1n
-           h1 = h10 + i * h1step
-           a = h1*l + h2*m + h3*n
-           I1  = a_o1v1 * cos(a)
-           I2  = a_o2v2 * cos(a)
-           I12 = a_o1v2 * cos(a)
-           intensity(i,j,k) = mu1*I1 + mu2*I2 + mu3*I12
-        end do
-     end do
-  end do
-end subroutine
+      ! print *, h1step, h2step, h3step
+
+      do k = 1,p%h3n
+         h3 = p%h30 + k * h3step
+         ! print *,  h3
+         do j = 1, p%h2n
+            h2 = p%h20 + j * h2step
+            do i = 1, p%h1n
+               h1 = p%h10 + i * h1step
+               a = h1*p%l + h2*p%m + h3*p%n
+               I1  = p%a_o1v1 * cos(a)
+               I2  = p%a_o2v2 * cos(a)
+               I12 = p%a_o1v2 * cos(a)
+               intensity(i,j,k) = p%mu1*I1 + p%mu2*I2 + p%mu3*I12
+            end do
+         end do
+      end do
+    end subroutine
+
+  end module
