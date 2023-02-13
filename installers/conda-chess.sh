437498e53409374d66b676289de47b794a96a6b7
Justin M Wozniak <woz@anl.gov>
2023-02-13 10:37:39 -0600

    Standard syntax

--- installers/conda-chess.sh
+++ installers/conda-chess.sh
@@ -5,8 +5,8 @@ CONDA=/nfs/chess/sw/nexpy/miniconda3-woz
 
 bash ~/downloads/Miniconda3-2022-07-28.sh -b -p $CONDA
 
-pt $CONDA
+PATH=$CONDA/bin:$PATH
 
-cicf gxx
-cicf hdf5 libtiff libjpeg libglu
-cicf qt qt-main qt-webengine qtconsole qtconsole-base qtpy
+conda install -c conda-forge gxx
+conda install -c conda-forge hdf5 libtiff libjpeg libglu
+conda install -c conda-forge qt qt-main qt-webengine qtconsole qtconsole-base qtpy
